8329b44b553cec5566e44f79adc8c11cba620769
Michael Staunton <mstaunto@umich.edu>
2018-12-01 18:55:31 -0500

    Database Schema Setup

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE upcoming(
+	betID INTEGER Primary Key,
+	gameID INTEGER NOT NULL,
+	bet_date VARCHAR(20) NOT NULL,
+	bet_time VARCHAR(20) NOT NULL,
+	game VARCHAR(40) NOT NULL,
+	selection VARCHAR(20) NOT NULL,
+	spread REAL NOT NULL,
+	odds INTEGER NOT NULL,
+	wager REAL NOT NULL,
+	to_win REAL NOT NULL
+);
